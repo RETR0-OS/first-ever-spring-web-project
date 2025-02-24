92bc0bf74dd6c8f5f352ea805aa0ebcb29ddbb72
RETR0-OS <aadityajindal12@gmail.com>
2025-02-24 00:24:58 -0700

    add methods for updated and create

--- springWeb/src/main/java/com/aaditya/honors/springWeb/controllers/ProductController.java
+++ springWeb/src/main/java/com/aaditya/honors/springWeb/controllers/ProductController.java
@@ -3,21 +3,38 @@ package com.aaditya.honors.springWeb.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.aaditya.honors.springWeb.services.ProductService;
 import com.aaditya.honors.springWeb.models.Product;
 
 
 @RestController
+@RequestMapping("/products")
 public class ProductController {
 
     @Autowired
     ProductService productService;
 
-    @GetMapping("/products")
+    @GetMapping("/")
     public List<Product> getProducts() {
         return productService.getProducts();
+    }
+
+    @GetMapping("/product/{id}")
+    public Product getProductById(@PathVariable int id) {
+        return productService.getProductByID(id);
+    }
+
+    @PostMapping("/add/new")
+    public String addProduct(@RequestBody Product product){
+        productService.addProduct(product);
+        return "Product added successfully";
+    }
+
+    @PutMapping("/update/{id}")
+    public String updateProduct(@PathVariable int id, @RequestBody Product product){
+        productService.updateProduct(id, product);
+        return "Product updated successfully";
     }
 }
--- springWeb/src/main/java/com/aaditya/honors/springWeb/services/ProductService.java
+++ springWeb/src/main/java/com/aaditya/honors/springWeb/services/ProductService.java
@@ -6,12 +6,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 import java.util.Arrays;
+import java.util.ArrayList;
 
 @Service
 public class ProductService {
-    private List<Product> products = Arrays.asList(new Product("Laptop", 850), new Product("Mobile", 999));
+    private ArrayList<Product> products = new ArrayList<>(Arrays.asList(new Product("Laptop", 850), new Product("Mobile", 999)));
     @GetMapping("/products")
     public List<Product> getProducts(){
         return products;
+    }
+
+    public Product getProductByID(int id) {
+        return products.stream().filter(product -> product.getId() == id).findFirst().get();
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void updateProduct(int id, Product product) {
+        Product target_prod = getProductByID(id);
+        target_prod.setName(product.getName());
+        target_prod.setPrice(product.getPrice());
     }
 }
